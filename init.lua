314c3b290bdd2430731113e1aaf2e9492f8910eb
takaneko <mail@takaneko.dev>
2024-10-25 11:30:08 +0900

    Add first init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- runtimepathの設定
+vim.opt.runtimepath:append(vim.fn.expand("$HOME/dotfiles"))
+
+-- vim-plugの初期化（vim-plugを継続して使用する場合）
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.nvim/plugged')
+-- plugin.vimの読み込み
+vim.cmd('runtime! vim/plugin.vim')
+vim.call('plug#end')
+
+-- 他の設定ファイルの読み込み
+vim.cmd('runtime! vim/basic.vim')
+vim.cmd('runtime! vim/plugsettings.vim')
--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ do
   ln -sf "$HOME/dotfiles/$file" "$HOME/$file"
 done
 
-ln -sf "$HOME/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
+# ln -sf "$HOME/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "$HOME/dotfiles/init.lua" "$HOME/.config/nvim/init.lua"
 ln -sf "$HOME/dotfiles/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
