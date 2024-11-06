1ba44f2c30ca2a3425ab1fa424bfb4c48795663c
takaneko <mail@takaneko.dev>
2024-11-06 23:25:46 +0900

    Disable treesitter-ruby

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,9 +16,6 @@ return {
           "gomod",       -- Go modules
           "gowork",      -- Go workspace
 
-          -- Ruby関連
-          "ruby",        -- Ruby
-
           -- 設定ファイル関連
           "lua",         -- Neovim設定用
           "vim",         -- Vim script
@@ -32,7 +29,7 @@ return {
         },
 
         -- 自動インストール
-        auto_install = true,
+        auto_install = false,
 
         -- ハイライトの設定
         highlight = {
@@ -61,20 +58,6 @@ return {
           enable = true,
         },
       }
-
-      -- TreeSitterを使用したFoldingの設定
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.opt.foldenable = false  -- 初期状態では折りたたみを無効化
-
-      -- ファイルタイプごとの追加設定
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "ruby", "eruby" },
-        callback = function()
-          -- Rubyのブロック内でのインデントを改善
-          vim.opt_local.indentkeys:append("=end,=elsif,=when,=ensure,=rescue,=else,=end")
-        end,
-      })
     end,
   },
 }
