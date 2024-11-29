859c920bba63ef1a200de1612a8ae2983a81023e
takaneko <mail@takaneko.dev>
2024-11-29 10:47:02 +0900

    feat: add telescope

--- lua/config/plugsettings.lua
+++ lua/config/plugsettings.lua
@@ -332,3 +332,10 @@ vim.api.nvim_create_user_command('IndentBlanklineToggle', function()
     enabled = not require("ibl.config").get_config(0).enabled,
   })
 end, {})
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
