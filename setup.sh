12b856b3a7c10225c7068fc50f593ee5ea42a347
takaneko <mail@takaneko.dev>
2024-12-10 23:18:29 +0900

    refactor: Remove init.vim and vimscript config

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .tmux.conf .bash_aliases .bash_profile .bashrc .gemrc .ctags .gitignore_global)
+DOT_FILES=(.tmux.conf .bash_aliases .bash_profile .bashrc .gemrc .ctags .gitignore_global)
 
 for file in "${DOT_FILES[@]}"
 do
   ln -sf "$HOME/dotfiles/$file" "$HOME/$file"
 done
 
-# ln -sf "$HOME/dotfiles/init.vim" "$HOME/.config/nvim/init.vim"
-# rm $HOME/.config/nvim/init.lua
 ln -sf "$HOME/dotfiles/init.lua" "$HOME/.config/nvim/init.lua"
-rm $HOME/.config/nvim/init.vim
 ln -sf "$HOME/dotfiles/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
