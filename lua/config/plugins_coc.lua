75f2936d1b01d2ebdd7254a6f4bb67fbc250d6fa
takaneko <mail@takaneko.dev>
2024-12-21 17:09:38 +0900

    fix(lspconfig): remove conflict keybind

--- lua/config/plugins_coc.lua
+++ lua/config/plugins_coc.lua
@@ -35,7 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
       keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
       -- Use <c-j> to trigger snippets
-      keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
+      -- keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
       -- Use <c-space> to trigger completion
       keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
       
