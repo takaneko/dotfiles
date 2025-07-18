689d0356ba5ab85ea8c5ddb8de82e191183a13fc
takaneko <mail@takaneko.dev>
2025-07-18 16:18:52 +0900

    fix: remove duplicate entry in DOT_FILES array

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.tmux.conf .bash_aliases .bash_profile .bashrc .gemrc .ctags .gitignore_global)
+DOT_FILES=(.tmux.conf .bash_aliases .bash_profile .bashrc .gemrc .gitignore_global)
 
 for file in "${DOT_FILES[@]}"
 do
